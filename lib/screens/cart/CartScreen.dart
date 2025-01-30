b8e18878773b927a282c581544ca35f1744c8e97
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-30 15:33:47 +0530

    Create CartScreen.dart

---
+++ lib/screens/cart/CartScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatefulWidget {
+
+  final String title;
+
+  const CartScreen({super.key, required this.title});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
