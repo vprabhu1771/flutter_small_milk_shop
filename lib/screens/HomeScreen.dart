ac717f202d7b2ed6986bbd39350bbc426bee4b0b
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-30 15:35:30 +0530

    HomeScreen Clip Path UI added

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -4,22 +4,85 @@ class HomeScreen extends StatefulWidget {
 
   final String title;
 
-  const HomeScreen({super.key, required this.title});
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(widget.title),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipPath(
+              clipper: CustomClipPath(),
+              child: Container(
+                color: Colors.green,
+                padding: EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      // Circle
+                      Positioned(
+                          top: -150,
+                          right: -250,
+                          child: Container(
+                            width: 400,
+                            height: 400,
+                            padding: EdgeInsets.all(0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(400),
+                                color: Colors.white.withOpacity(0.1)
+                            ),
+                          )
+                      ),
+                      // Circle
+                      Positioned(
+                          top: 100,
+                          right: -300,
+                          child: Container(
+                            width: 400,
+                            height: 400,
+                            padding: EdgeInsets.all(0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(400),
+                                color: Colors.white.withOpacity(0.1)
+                            ),
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+
+          ],
+        ),
       ),
     );
   }
+}
+
+class CustomClipPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 50);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 50);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
