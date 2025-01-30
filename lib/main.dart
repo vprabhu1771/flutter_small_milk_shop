3d85100817c96a3796661aab3d7f8a4d6ea90f1a
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-30 15:49:14 +0530

    HomePage added to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_small_milk_shop/screens/HomePage.dart';
 import 'package:flutter_small_milk_shop/screens/HomeScreen.dart';
 import 'package:flutter_small_milk_shop/screens/ProductScreen.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(title: 'Home'),
+      home: HomePage(),
     );
   }
 }
