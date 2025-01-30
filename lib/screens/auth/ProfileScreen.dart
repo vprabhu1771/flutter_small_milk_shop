622f6c86f90968518f4b59fe259b0079d2a46bc3
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-30 15:48:49 +0530

    Create ProfileScreen.dart

---
+++ lib/screens/auth/ProfileScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+
+  final String title;
+
+  const ProfileScreen({super.key, required this.title});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
