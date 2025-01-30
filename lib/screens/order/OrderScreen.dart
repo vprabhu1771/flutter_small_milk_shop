4c17a262fd2a4b291e1e1f800f855d085cf73091
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-30 15:33:52 +0530

    Create OrderScreen.dart

---
+++ lib/screens/order/OrderScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatefulWidget {
+
+  final String title;
+
+  const OrderScreen({super.key, required this.title});
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
