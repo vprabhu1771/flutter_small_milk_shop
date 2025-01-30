7f8038ba29b71df80ded9516139fe4263e34d148
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-30 15:20:08 +0530

    Create RegisterScreen.dart

---
+++ lib/screens/auth/RegisterScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+
+  final String title;
+
+  const RegisterScreen({super.key, required this.title});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
